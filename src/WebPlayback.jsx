52ed4d1fe6d49ce330bdaf1dd833c8c9170b204f
Daniele ventura <danieleventuraa@gmail.com>
2024-01-16 10:38:29 -0300

    add download image

--- src/WebPlayback.jsx
+++ src/WebPlayback.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Note from './components/Note';
 import Notepad from './components/Notepad';
 import Footer from './components/Footer';
@@ -14,6 +14,10 @@ import User from './types/User';
 import track from './types/Track';
 import { api } from './api';
 
+import { saveAs } from 'file-saver';
+import CanvasToBlob from 'canvas-toBlob/canvas-toBlob.js'
+import html2canvas from 'html2canvas';
+
 function WebPlayback(props) {
 
     const [is_paused, setPaused] = useState(false);
@@ -26,7 +30,10 @@ function WebPlayback(props) {
     const [tracks, setTracks] = useState([]);
     const [current_playlist_uri, setCurrentPlaylistURI] = useState("");
     const [current_track_uri, setCurrentTrackURI] = useState("");
+    const [inputTrack, setInputTrack] = useState("");
+    const [inputPlaylist, setInputPlaylist] = useState("");
     const [is_shuffle, setShuffle] = useState(false);
+    const elementToCaptureRef = useRef(null);
 
     const getTracks = async (playlist_id) => {
         setTracks([]);
@@ -43,6 +50,7 @@ function WebPlayback(props) {
             if (item.id == playlist_id) {
                 setCurrentPlaylistURI(item.uri);
                 setPLaylist(item.name);
+                setInputPlaylist(item.name);
             }
         });
         let next = "nextTrack";
@@ -61,12 +69,11 @@ function WebPlayback(props) {
 
     const getUserSavedTracks = async () => {
         setTracks([]);
-        console.log(user)
-
         if (user.uri != null) {
             var uri = user.uri + ":collection";
             setCurrentPlaylistURI(uri);
             setPLaylist("Liked Songs");
+            setInputPlaylist("Liked Songs");
         }
 
         let next = "nextTrack";
@@ -95,7 +102,6 @@ function WebPlayback(props) {
     };
 
     const playTrack = async (e) => {
-        console.log("uir: ", current_playlist_uri)
         setCurrentTrackURI(e.target.value);
         let response = await api.setTrack(props.token, e.target.value, current_playlist_uri);
     };
@@ -119,9 +125,12 @@ function WebPlayback(props) {
             if (metadata.context_description) {
                 //setCurrentPlaylistURI(metadata.uri);
                 setPLaylist(metadata.context_description);
+                setInputPlaylist(metadata.context_description);
+                setInputTrack(state.track_window.current_track.name);
                 //getTracksPlaylist(playlist_id[2]);
             } else {
                 setPLaylist("Liked Songs");
+                setInputPlaylist("Liked Songs");
                 //getUserSavedTracks();
             }
         };
@@ -171,19 +180,63 @@ function WebPlayback(props) {
                 }
 
                 setTrack(state.track_window.current_track);
+                setInputTrack(state.track_window.current_track.name);
                 setPaused(state.paused);
 
                 player.getCurrentState().then(state => {
                     (!state) ? setActive(false) : setActive(true)
                     //config state current
                     setPlaybackSate(state);
-                    console.log(state)
                 });
             }));
 
             player.connect();
         };
     }, []);
+
+    async function download() {
+
+        const elTrack = document.getElementById("tracks");
+        const elPlaylist = document.getElementById("playlist");
+
+        const elInputTrack = document.getElementById("input-track");
+        const elInputPlaylist = document.getElementById("input-playlist");
+
+        const elRange = document.getElementById("range");
+        const elFakeRange = document.getElementById("fake-range");
+
+        try {
+
+            elTrack.style.display = "none";
+            elPlaylist.style.display = "none";
+            elInputTrack.style.display = "block";
+            elInputPlaylist.style.display = "block";
+            elRange.style.display = "none";
+            elFakeRange.style.display = "block";
+
+            html2canvas(elementToCaptureRef.current, {
+                logging: true,
+                useCORS: true,
+                allowTaint: true,
+                taintTest: true,
+                letterRendering: 1
+            }).then((canvas) => {
+                canvas.toBlob((blob) => {
+                    track ? saveAs(blob, current_track.name + '-player.png') : window.saveAs(blob, 'player.png');
+                });
+            });
+        } catch (error) {
+            alert("Error capturing or saving the image")
+            console.error('Error capturing or saving the image:', error);
+        } finally {
+            elTrack.style.display = "block";
+            elPlaylist.style.display = "block";
+            elInputTrack.style.display = "none";
+            elInputPlaylist.style.display = "none";
+            elRange.style.display = "block";
+            elFakeRange.style.display = "none";
+        }
+    }
 
     if (!is_active) {
         return (
@@ -196,64 +249,72 @@ function WebPlayback(props) {
     } else {
         return (
             <>
-                <div class="box player">
-                    <div class="title music">
-                        <img src={icon} width="20" height="20" class="icon-music" />
-                        <p class="title music">Music Player</p>
-                    </div>
-                    <div class="body">
-                        <div class="container-info-music">
-                            <img src={current_track.album.images[0].url} alt="" class="photo-album" />
-                            <div class="box-spotify">
-                                <div class="music-group">
-                                    <p>Playlist:
-                                        <select name="playlists" id="playlist" onChange={(e) => { getTracks(e.target.value), document.getElementById("playlist").value = "0"; }}>
-                                            <option value="0" class="option-music" selected disabled>{current_playlist}</option>
-                                            <option value="1" >Liked Songs</option>
-                                            {playlists.map(item =>
-                                                <option key={item.uri} value={item.id}>{item.name}</option>
-                                            )};
-                                        </select>
-                                    </p>
-                                </div>
-                                <div class="music-group">
-                                    <p>Track:
-                                        <select value={0} name="tracks" id="tracks" onChange={(e) => { playTrack(e) }}>
-                                            <option value="0" class="option-music" selected disabled>{current_track.name}</option>
-                                            {tracks.map(item =>
-                                                <option value={item.track.uri}>{item.track.name}</option>
-                                            )};
-                                        </select>
-                                    </p>
-                                </div>
-                                <div class="music-group">
-                                    <p>Artist: <span id="artist">{current_track.artists[0].name}</span></p>
-                                </div>
-                                <div class="music-group">
-                                    <div class="slider"><input type="range" min="0" max="100" onChange={(e) => { player.setVolume(e.target.value / 100) }}></input></div>
-                                </div>
-                                <div class="bnt-spotify">
-                                    <button onClick={() => { player.previousTrack() }} >
-                                        <img src={iconPrevious} class="icon-control-player"></img>
-                                    </button>
+                <div class="container">
+                    <div class="box player" id="card" ref={elementToCaptureRef}>
+                        <div class="title music">
+                            <img src={icon} width="20" height="20" class="icon-music" />
+                            <p class="title music">Music Player</p>
+                        </div>
+                        <div class="body">
+                            <div class="container-info-music" >
+                                <img src={current_track.album.images[0].url} alt="" class="photo-album" />
+                                <div class="box-spotify">
+                                    <div class="music-group">
+                                        <p>Playlist:
+                                            <div id="input-playlist" class="input-player" >{inputPlaylist}</div>
+                                            <select name="playlists" id="playlist" onChange={(e) => { getTracks(e.target.value), document.getElementById("playlist").value = "0"; setInputPlaylist(current_playlist); }}>
+                                                <option value="0" class="option-music" selected disabled>{current_playlist}</option>
+                                                <option value="1" >Liked Songs</option>
+                                                {playlists.map(item =>
+                                                    <option key={item.uri} value={item.id}>{item.name}</option>
+                                                )};
+                                            </select>
+                                        </p>
+                                    </div>
+                                    <div class="music-group">
+                                        <p>Track:
+                                            <div id="input-track" class="input-player tracks">{inputTrack}</div>
+                                            <select value={0} name="tracks" id="tracks" onChange={(e) => { playTrack(e) }}>
+                                                <option value="0" class="option-music" selected disabled>{current_track.name}</option>
+                                                {tracks.map(item =>
+                                                    <option value={item.track.uri}>{item.track.name}</option>
+                                                )};
+                                            </select>
+                                        </p>
+                                    </div>
+                                    <div class="music-group">
+                                        <p>Artist: <span id="artist">{current_track.artists[0].name}</span></p>
+                                    </div>
+                                    <div class="music-group">
+                                        <div>
+                                            <div id="fake-range" class="slider"><div class="custom-range">
+                                                <div class="custom-thumb"></div></div>
+                                            </div>
+                                            <input id="range" type="range" min="0" max="100" onChange={(e) => { player.setVolume(e.target.value / 100) }}></input>
+                                        </div>
+                                    </div>
+                                    <div class="bnt-spotify">
+                                        <button onClick={() => { player.previousTrack() }} >
+                                            <img src={iconPrevious} class="icon-control-player"></img>
+                                        </button>
 
-                                    <button onClick={() => { player.togglePlay() }} >
-                                        {is_paused ? <img src={iconPlay} class="icon-control-player"></img> : <img src={iconPause} class="icon-control-player"></img>}
-                                    </button>
+                                        <button onClick={() => { player.togglePlay() }} >
+                                            {is_paused ? <img src={iconPlay} class="icon-control-player"></img> : <img src={iconPause} class="icon-control-player"></img>}
+                                        </button>
 
-                                    <button onClick={() => { player.nextTrack() }} >
-                                        <img src={iconNext} class="icon-control-player"></img>
-                                    </button>
+                                        <button onClick={() => { player.nextTrack() }} >
+                                            <img src={iconNext} class="icon-control-player"></img>
+                                        </button>
 
-                                    <button id="btn-shuffle" class="active" onClick={() => { changeShuffle() }} >
-                                        {is_shuffle ? <img src={iconShuffle} class="icon-control-player"></img> : <img src={iconShuffle} class="icon-control-player"></img>}
-                                    </button>
+                                        <button id="btn-shuffle" class="active" onClick={() => { changeShuffle() }} >
+                                            {is_shuffle ? <img src={iconShuffle} class="icon-control-player"></img> : <img src={iconShuffle} class="icon-control-player"></img>}
+                                        </button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-
-
                     </div>
+                    <button onClick={download} > Download </button>
                 </div>
             </>
         );
